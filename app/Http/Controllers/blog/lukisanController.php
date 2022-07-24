5e0a295a51d19b73a2ee801c07b8115b17aee7de
unknown <ubaidillah.faris99@gmail.com>
2022-07-24 20:29:00 +0700

    fix customer

--- app/Http/Controllers/blog/lukisanController.php
+++ app/Http/Controllers/blog/lukisanController.php
@@ -14,20 +14,28 @@ class lukisanController extends Controller
       ->get();
       foreach ($get as $key => $value) {
         $template .= '
-        <div class="col-lg-4 col-md-6 portfolio-item filter-'.$value->kategori.'">
-          <div class="portfolio-wrap">
-            <img src="'.asset('public/images').'/'.$value->gambar.'" class="img-fluid" alt="">
-            <div class="portfolio-info">
-              <h4>'.$value->nama.'</h4>
-              <p>'.$value->kategori.'</p>
-              <div class="portfolio-links">
-                <a href="'.asset('public/images').'/'.$value->gambar.'" data-gallery="portfolioGallery" class="portfolio-lightbox" title="Web 3"><i class="fa fa-eye" aria-hidden="true"></i></a>
-                <a href="'.route('blog.lukisan.detail',['id'=>$value->id]).'" title="More Details"><i class="fa fa-shopping-bag" aria-hidden="true"></i></a>
+        <div class="col-md-4 col-lg-4 grid-margin stretch-card">
+        <div class="card card-rounded">
+          <div class="card-body">
+              <div class="portfolio-wrap">
+                <img src="'.asset('public/images').'/'.$value->gambar.'" class="img-fluid" alt="">
+                <div class="portfolio-info">
+                  <hr>
+                  <p class="h4">'.$value->nama.'</p>
+                  <p>'.$value->kategori.'</p>
+                </div>
+              </div>
+
+            <div class="list align-items-center pt-3">
+              <div class="wrapper w-100">
+                <p class="mb-0">
+                  <a href="#" class="fw-bold text-primary">Show all <i class="mdi mdi-arrow-right ms-2"></i></a>
+                </p>
               </div>
             </div>
           </div>
         </div>
-        ';
+        </div>';
       }
       return response()->json($template,200);
     }
--- resources/views/layouts/blog/app.blade.php
+++ resources/views/layouts/blog/app.blade.php
@@ -10,30 +10,205 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="{{asset('public\bethany\assets\img\favicon.png')}}" rel="icon">
-  <link href="{{asset('public\bethany\assets\img\apple-touch-icon.png')}}" rel="apple-touch-icon">
-
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="{{asset('public\bethany\assets\vendor\aos\aos.css')}}" rel="stylesheet">
-  <link href="{{asset('public\bethany\assets\vendor\bootstrap\css\bootstrap.min.css')}}" rel="stylesheet">
-  <link href="{{asset('public\bethany\assets\vendor\bootstrap-icons\bootstrap-icons.css')}}" rel="stylesheet">
-  <link href="{{asset('public\bethany\assets\vendor\boxicons\css\boxicons.min.css')}}" rel="stylesheet">
-  <link href="{{asset('public\bethany\assets\vendor\glightbox\css\glightbox.min.css')}}" rel="stylesheet">
-  <link href="{{asset('public\bethany\assets\vendor\remixicon\remixicon.css')}}" rel="stylesheet">
-  <link href="{{asset('public\bethany\assets\vendor\swiper\swiper-bundle.min.css')}}" rel="stylesheet">
-  <link rel="stylesheet" href="{{asset('public\sb_admin\vendor\fontawesome-free\css\all.min.css')}}">
-
-  <!-- Template Main CSS File -->
-  <link href="{{asset('public\bethany\assets\css\style.css')}}" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('public/template/vendors/feather/feather.css')}}">
+  <link rel="stylesheet" href="{{asset('public/template/vendors/mdi/css/materialdesignicons.min.css')}}">
+  <link rel="stylesheet" href="{{asset('public/template/vendors/ti-icons/css/themify-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('public/template/vendors/typicons/typicons.css')}}">
+  <link rel="stylesheet" href="{{asset('public/template/vendors/simple-line-icons/css/simple-line-icons.css')}}">
+  <link rel="stylesheet" href="{{asset('public/template/vendors/css/vendor.bundle.base.css')}}">
+  <!-- endinject -->
+  <!-- Plugin css for this page -->
+  <link rel="stylesheet" href="{{asset('public/template/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
+  <link rel="stylesheet" href="{{asset('public/template/js/select.dataTables.min.css')}}">
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="{{asset('public/template/css/vertical-layout-light/style.css')}}">
+  <link rel="shortcut icon" href="{{asset('public/template/images/favicon.png')}}" />
 </head>
 
 <body>
-
+  <div class="container-scroller">
+    @include('layouts.blog.navbar')
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_settings-panel.html -->
+      <div id="right-sidebar" class="settings-panel">
+        <i class="settings-close ti-close"></i>
+        <ul class="nav nav-tabs border-top" id="setting-panel" role="tablist">
+          <li class="nav-item">
+            <a class="nav-link active" id="todo-tab" data-bs-toggle="tab" href="#todo-section" role="tab" aria-controls="todo-section" aria-expanded="true">TO DO LIST</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" id="chats-tab" data-bs-toggle="tab" href="#chats-section" role="tab" aria-controls="chats-section">CHATS</a>
+          </li>
+        </ul>
+        <div class="tab-content" id="setting-content">
+          <div class="tab-pane fade show active scroll-wrapper" id="todo-section" role="tabpanel" aria-labelledby="todo-section">
+            <div class="add-items d-flex px-3 mb-0">
+              <form class="form w-100">
+                <div class="form-group d-flex">
+                  <input type="text" class="form-control todo-list-input" placeholder="Add To-do">
+                  <button type="submit" class="add btn btn-primary todo-list-add-btn" id="add-task">Add</button>
+                </div>
+              </form>
+            </div>
+            <div class="list-wrapper px-3">
+              <ul class="d-flex flex-column-reverse todo-list">
+                <li>
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input class="checkbox" type="checkbox">
+                      Team review meeting at 3.00 PM
+                    </label>
+                  </div>
+                  <i class="remove ti-close"></i>
+                </li>
+                <li>
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input class="checkbox" type="checkbox">
+                      Prepare for presentation
+                    </label>
+                  </div>
+                  <i class="remove ti-close"></i>
+                </li>
+                <li>
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input class="checkbox" type="checkbox">
+                      Resolve all the low priority tickets due today
+                    </label>
+                  </div>
+                  <i class="remove ti-close"></i>
+                </li>
+                <li class="completed">
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input class="checkbox" type="checkbox" checked>
+                      Schedule meeting for next week
+                    </label>
+                  </div>
+                  <i class="remove ti-close"></i>
+                </li>
+                <li class="completed">
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input class="checkbox" type="checkbox" checked>
+                      Project review
+                    </label>
+                  </div>
+                  <i class="remove ti-close"></i>
+                </li>
+              </ul>
+            </div>
+            <h4 class="px-3 text-muted mt-5 fw-light mb-0">Events</h4>
+            <div class="events pt-4 px-3">
+              <div class="wrapper d-flex mb-2">
+                <i class="ti-control-record text-primary me-2"></i>
+                <span>Feb 11 2018</span>
+              </div>
+              <p class="mb-0 font-weight-thin text-gray">Creating component page build a js</p>
+              <p class="text-gray mb-0">The total number of sessions</p>
+            </div>
+            <div class="events pt-4 px-3">
+              <div class="wrapper d-flex mb-2">
+                <i class="ti-control-record text-primary me-2"></i>
+                <span>Feb 7 2018</span>
+              </div>
+              <p class="mb-0 font-weight-thin text-gray">Meeting with Alisa</p>
+              <p class="text-gray mb-0 ">Call Sarah Graves</p>
+            </div>
+          </div>
+          <!-- To do section tab ends -->
+          <div class="tab-pane fade" id="chats-section" role="tabpanel" aria-labelledby="chats-section">
+            <div class="d-flex align-items-center justify-content-between border-bottom">
+              <p class="settings-heading border-top-0 mb-3 pl-3 pt-0 border-bottom-0 pb-0">Friends</p>
+              <small class="settings-heading border-top-0 mb-3 pt-0 border-bottom-0 pb-0 pr-3 fw-normal">See All</small>
+            </div>
+            <ul class="chat-list">
+              <li class="list active">
+                <div class="profile"><img src="images/faces/face1.jpg" alt="image"><span class="online"></span></div>
+                <div class="info">
+                  <p>Thomas Douglas</p>
+                  <p>Available</p>
+                </div>
+                <small class="text-muted my-auto">19 min</small>
+              </li>
+              <li class="list">
+                <div class="profile"><img src="images/faces/face2.jpg" alt="image"><span class="offline"></span></div>
+                <div class="info">
+                  <div class="wrapper d-flex">
+                    <p>Catherine</p>
+                  </div>
+                  <p>Away</p>
+                </div>
+                <div class="badge badge-success badge-pill my-auto mx-2">4</div>
+                <small class="text-muted my-auto">23 min</small>
+              </li>
+              <li class="list">
+                <div class="profile"><img src="images/faces/face3.jpg" alt="image"><span class="online"></span></div>
+                <div class="info">
+                  <p>Daniel Russell</p>
+                  <p>Available</p>
+                </div>
+                <small class="text-muted my-auto">14 min</small>
+              </li>
+              <li class="list">
+                <div class="profile"><img src="images/faces/face4.jpg" alt="image"><span class="offline"></span></div>
+                <div class="info">
+                  <p>James Richardson</p>
+                  <p>Away</p>
+                </div>
+                <small class="text-muted my-auto">2 min</small>
+              </li>
+              <li class="list">
+                <div class="profile"><img src="images/faces/face5.jpg" alt="image"><span class="online"></span></div>
+                <div class="info">
+                  <p>Madeline Kennedy</p>
+                  <p>Available</p>
+                </div>
+                <small class="text-muted my-auto">5 min</small>
+              </li>
+              <li class="list">
+                <div class="profile"><img src="images/faces/face6.jpg" alt="image"><span class="online"></span></div>
+                <div class="info">
+                  <p>Sarah Graves</p>
+                  <p>Available</p>
+                </div>
+                <small class="text-muted my-auto">47 min</small>
+              </li>
+            </ul>
+          </div>
+          <!-- chat tab ends -->
+        </div>
+      </div>
+      <!-- partial -->
+      @include('layouts.blog.sidebar')
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+                @yield('content')
+          </div>
+        </div>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+        <footer class="footer">
+          <div class="d-sm-flex justify-content-center justify-content-sm-between">
+            <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash.</span>
+            <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
+          </div>
+        </footer>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
   <!-- ======= Header ======= -->
-  <header id="header" class="fixed-top d-flex align-items-center">
+  {{-- <header id="header" class="fixed-top d-flex align-items-center">
     <div class="container">
       <div class="header-container d-flex align-items-center justify-content-between">
         <div class="logo">
@@ -42,127 +217,40 @@
           <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
         </div>
 
-        <nav id="navbar" class="navbar">
-          <ul>
-            <li><a class="nav-link scrollto active" href="{{route('blog.index')}}">Beranda</a></li>
-            @auth
-              <li><a class="nav-link scrollto" href="{{route('blog.keranjang.index')}}">Keranjang</a></li>
-              <li><a class="nav-link scrollto" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"><i class="fa fa-sign-in" aria-hidden="true"></i> Logout</a></li>
-              <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                  @csrf
-              </form>
-            @endauth
-            @guest
-              <li><a class="nav-link scrollto" href="{{route('login')}}"><i class="fa fa-sign-in" aria-hidden="true"></i> Login</a></li>
-              <li><a class="nav-link scrollto" href="{{route('register')}}">Register</a></li>
-            @endguest
-            <li><a class="getstarted nav-link scrollto" href="#portfolio">Karya</a></li>
-          </ul>
-          <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav><!-- .navbar -->
+        <!-- .navbar -->
 
       </div><!-- End Header Container -->
     </div>
-  </header><!-- End Header -->
+  </header><!-- End Header --> --}}
 
   <!-- ======= Hero Section ======= -->
 
 
-    <main id="main">
-      @yield('content')
-    </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
 
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Bethany</h3>
-            <p>
-              A108 Adam Street <br>
-              New York, NY 535022<br>
-              United States <br><br>
-              <strong>Phone:</strong> +1 5589 55488 55<br>
-              <strong>Email:</strong> info@example.com<br>
-            </p>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Join Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
-          </div>
-
-        </div>
-      </div>
-    </div>
-
-    <div class="container d-md-flex py-4">
-
-      <div class="me-md-auto text-center text-md-start">
-        <div class="copyright">
-          &copy; Copyright <strong><span>Bethany</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/bethany-free-onepage-bootstrap-theme/ -->
-          Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-        </div>
-      </div>
-      <div class="social-links text-center text-md-right pt-3 pt-md-0">
-        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-        <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
 
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-  <script src="{{asset('public\sb_admin\vendor\jquery\jquery.min.js')}}"></script>
 
   <!-- Vendor JS Files -->
-  <script src="{{asset('public\bethany\assets\vendor/purecounter/purecounter.js')}}"></script>
-  <script src="{{asset('public\bethany\assets/vendor/aos/aos.js')}}"></script>
-  <script src="{{asset('public\bethany\assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-  <script src="{{asset('public\bethany\assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
-  <script src="{{asset('public\bethany\assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
-  <script src="{{asset('public\bethany\assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
-  <script src="{{asset('public\bethany\assets/vendor/php-email-form/validate.js')}}"></script>
-  <script src="{{asset('public\sweetalert2\dist\sweetalert2.all.min.js')}}"></script>
+  <script src="{{asset('public/template/vendors/js/vendor.bundle.base.js')}}"></script>
+  <!-- endinject -->
+  <!-- Plugin js for this page -->
+  <script src="{{asset('public/template/vendors/chart.js/Chart.min.js')}}"></script>
+  <script src="{{asset('public/template/vendors/bootstrap-datepicker/bootstrap-datepicker.min.js')}}"></script>
+  <script src="{{asset('public/template/vendors/progressbar.js/progressbar.min.js')}}"></script>
 
-  <!-- Template Main JS File -->
-  <script src="{{asset('public\bethany\assets/js/main.js')}}"></script>
+  <!-- End plugin js for this page -->
+  <!-- inject:js -->
+  <script src="{{asset('public/template/js/off-canvas.js')}}"></script>
+  <script src="{{asset('public/template/js/hoverable-collapse.js')}}"></script>
+  <script src="{{asset('public/template/js/template.js')}}"></script>
+  <script src="{{asset('public/template/js/settings.js')}}"></script>
+  <script src="{{asset('public/template/js/todolist.js')}}"></script>
+  <!-- endinject -->
+  <!-- Custom js for this page-->
+  <script src="{{asset('public/template/js/jquery.cookie.js" type="text/javascript')}}"></script>
+  <script src="{{asset('public/template/js/dashboard.js')}}"></script>
+  <script src="{{asset('public/template/js/Chart.roundedBarCharts.js')}}"></script>
 
   @stack('script')
 </body>
